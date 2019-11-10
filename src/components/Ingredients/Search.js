6b722a858ed4140febb4a24ed63852aa2d4f4116
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-10 22:32:01 +0530

    added custom hook in search file

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -4,42 +4,50 @@ import Card from '../UI/Card';
 import TextInput from '../UI/TextInput'
 import './Search.css';
 
+import ErrorModal from '../UI/ErrorModal';
+
+import useHttp from '../../hooks/http';
+
 const Search = memo(props => {
   const { onLoadIngredients } = props
   const [enteredFilter, setEnteredFilter] = useState('')
+  const { isLoading, data, error, sendRequest, clear } = useHttp();
   const inputRef = useRef();
   useEffect(() => {
     setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`
-        fetch('https://hooks-01.firebaseio.com/ingredients.json' + query).then(response => {
-          return response.json();
-        }).then(respnseData => {
-          const loadedIngredients = [];
-          for (const key in respnseData) {
-            loadedIngredients.push({
-              id: key,
-              title: respnseData[key].title,
-              amount: respnseData[key].amount
-            })
-          }
-          onLoadIngredients(loadedIngredients)
-        })
+        sendRequest(`https://hooks-01.firebaseio.com/ingredients.json${query}`, `GET`);
       }
     }, 500)
 
-  }, [enteredFilter, onLoadIngredients, inputRef])
+  }, [enteredFilter, sendRequest, inputRef])
   const handleChange = e => {
     const { id, value } = e.target;
     if (id === 'filter') {
       setEnteredFilter(value)
     }
   }
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        })
+      }
+      onLoadIngredients(loadedIngredients)
+    }
+  }, [isLoading, data, error, onLoadIngredients])
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label htmlFor='filter' >Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <TextInput refe={inputRef} id='filter' type="text" value={enteredFilter} onChange={handleChange} />
         </div>
       </Card>
